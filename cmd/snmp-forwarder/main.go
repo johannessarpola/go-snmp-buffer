96f0fdab28c892645cc912b89af4215d21a834b5
Johannes Sarpola <johannes.sarpola@gmail.com>
2023-03-28 20:43:32 +0300

    hacked together forwarding from the embedded db

--- cmd/snmp-forwarder/main.go
+++ cmd/snmp-forwarder/main.go
@@ -2,24 +2,72 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"encoding/gob"
+	"fmt"
+	"log"
 
-	"github.com/dgraph-io/badger"
+	"github.com/dgraph-io/badger/v4"
+	"github.com/dgraph-io/ristretto/z"
+	db "github.com/johannessarpola/go-network-buffer/db"
+	"github.com/johannessarpola/go-network-buffer/models"
+	"github.com/johannessarpola/go-network-buffer/utils"
 )
 
 func main() {
 	// TODO Read SNMP from disk -> send forward with some adapter(?)
-	print("Hello world")
-	stream := db.NewStream()
-	// db.NewStreamAt(readTs) for managed mode.
+
+	data := db.NewData("../../_tmp", "snmp_") // Will cause conflict probably if run with listener
+	stream := data.DB.NewStream()
 
 	// -- Optional settings
-	stream.NumGo = 16                     // Set number of goroutines to use for iteration.
-	stream.Prefix = []byte("some-prefix") // Leave nil for iteration over the whole DB.
-	stream.LogPrefix = "Badger.Streaming" // For identifying stream logs. Outputs to Logger.
+	stream.NumGo = 8                    // Set number of goroutines to use for iteration.
+	stream.Prefix = []byte("snmp_")     // Leave nil for iteration over the whole DB.
+	stream.LogPrefix = "snmp.Streaming" // For identifying stream logs. Outputs to Logger.
 
 	// ChooseKey is called concurrently for every key. If left nil, assumes true by default.
-	stream.ChooseKey = func(item *badger.Item) bool {
-		return bytes.HasSuffix(item.Key(), []byte("er"))
+	// TODO Filtering does not seem to be working for now
+	// stream.ChooseKey = func(item *badger.Item) bool {
+	// 	return bytes.HasSuffix(item.Key(), []byte("snmp_"))
+	// }
+
+	// Send is called serially, while Stream.Orchestrate is running.
+	// stream.Send = c.Send // TODO Fix at some point
+	stream.Send = func(buf *z.Buffer) error {
+		list, err := badger.BufferToKVList(buf)
+		if err != nil {
+			return err
+		}
+		for _, kv := range list.Kv {
+			if kv.StreamDone == true {
+				return nil
+			}
+
+			// TODO Clean up at some point
+			prefix := []byte("snmp_")                        // TODO Ugly as f
+			k := utils.ConvertToUint64(kv.Key[len(prefix):]) // TODO Ugly as f
+			fmt.Printf("key: %d\n", k)
+			v := kv.Value
+
+			var p models.Packet
+			buf := bytes.NewBuffer(v)
+			decoder := gob.NewDecoder(buf)
+			err := decoder.Decode(&p)
+			if err != nil {
+				log.Println("could not decode!!")
+			}
+			println(p.Community)
+			for _, v := range p.Variables {
+				println(fmt.Sprintf("%s,%s,%s", v.Name, v.Type.String(), v.Value))
+			}
+
+		}
+		return nil // TODO Needs some handling
+	}
+
+	// Run the stream
+	if err := stream.Orchestrate(context.Background()); err != nil {
+		log.Fatal("Apuva")
 	}
 
 }
