f9868c2451b3b6b9cea7e925252c70a170059c33
Johannes Sarpola <johannes.sarpola@gmail.com>
2023-03-30 12:59:46 +0300

    add couple more meths to index

--- models/index.go
+++ models/index.go
@@ -1,39 +1,55 @@
 package models
 
+import "github.com/johannessarpola/go-network-buffer/utils"
+
 type Index struct {
-	name   string
-	value  uint64
-	exists bool
+	Name   string
+	Value  uint64
+	Exists bool
 }
 
 func NewIndex(name string, value uint64) Index {
 	return Index{
-		name:   name,
-		value:  value,
-		exists: true,
+		Name:   name,
+		Value:  value,
+		Exists: true,
 	}
 }
 
-func EmptyIndex(name string) Index {
+func ZeroIndex(name string) Index {
 	return Index{
-		name:   name,
-		value:  0,
-		exists: false,
+		Name:   name,
+		Value:  0,
+		Exists: false,
 	}
+}
+
+func (idx *Index) AsBytes() ([]byte, []byte) {
+	return idx.KeyAsBytes(), idx.ValueAsBytes()
+}
+
+func (idx *Index) KeyAsBytes() []byte {
+	return []byte(idx.Name)
+}
+
+func (idx *Index) ValueAsBytes() []byte {
+	return utils.ConvertToByteArr(idx.Value)
 }
 
 func (idx *Index) WithValue(value uint64) *Index {
-	idx.value = value
+	idx.Value = value
 	return idx
 }
 
 func (idx *Index) WithExists(exists bool) *Index {
-	idx.exists = exists
+	idx.Exists = exists
 	return idx
 }
 
 func (idx *Index) SetValue(value uint64) *Index {
-	return idx
-		.WithValue(value)
-		.WithExists(true)
+	return idx.WithValue(value).WithExists(true)
+}
+
+func (idx *Index) Increment() *Index {
+	return idx.SetValue(idx.Value + 1)
 }
