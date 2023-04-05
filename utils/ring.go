06088d36c1e4e170696e7d282fed34c0d5835d53
johannessarpola <johannes.sarpola@elisa.fi>
2023-04-05 11:13:25 +0300

    add ringbuffer interface

---
+++ utils/ring.go
@@ -0,0 +1,38 @@
+package utils
+
+type RingBuffer interface {
+	/*
+		Set the maximum size of the ring buffer.
+	*/
+	SetCapacity(size int)
+	/*
+		Capacity returns the current capacity of the ring buffer.
+	*/
+	Capacity() int
+	/*
+		ContentSize returns the current number of elements inside the ring buffer.
+	*/
+	ContentSize() int
+	/*
+		Enqueue a value into the Ring buffer.
+	*/
+	Enqueue(i interface{})
+	/*
+		Dequeue a value from the Ring buffer.
+
+		Returns nil if the ring buffer is empty.
+	*/
+	Dequeue() interface{}
+	/*
+		Values returns a slice of all the values in the circular buffer without modifying them at all.
+		The returned slice can be modified independently of the circular buffer. However, the values inside the slice
+		are shared between the slice and circular buffer.
+	*/
+	Values() []interface{}
+	/*
+		Read the value that Dequeue would have dequeued without actually dequeuing it.
+
+		Returns nil if the ring buffer is empty.
+	*/
+	Peek() interface{}
+}
